75c982984dde82dc4cd676fb0cab9a85e12623cb
PabloMunizMontoya <pablomunizmontoya@gmail.com>
2023-01-17 22:09:20 -0500

    aregle media queri y estilos ccs

--- src/routes/Comics/maincomics/Maincomics.jsx
+++ src/routes/Comics/maincomics/Maincomics.jsx
@@ -1,52 +1,47 @@
-
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./maincomics.css";
-import  ComicsCategories  from "../../../components/comics/ComicsCategories";
-import {  useDispatch, useSelector } from "react-redux";
+import ComicsCategories from "../../../components/comics/ComicsCategories";
+import { useDispatch, useSelector } from "react-redux";
 import comicsActions from "../../../store/comics/actions";
 import ComicsCards from "../../../components/comics/ComicsCards";
-const {getComics} = comicsActions
+const { getComics } = comicsActions;
 
+const Maincomics = () => {
+    const comicStore = useSelector((store) => store.comics.comics);
+    console.log(comicStore);
+    const text = useSelector((store) => store.comics.text);
+    useSelector((store) => store.comics);
+    const inputCategory = useSelector(
+        (store) => store.filterCategoryComic.filterCategory
+    );
 
- const Maincomics =()  =>{
-    const comicStore = useSelector((store) => store.comics.comics)
-    console.log(comicStore)
-    const text = useSelector((store) => store.comics.text)
-    useSelector((store) => store.comics)
-    const inputCategory = useSelector((store) => store.filterCategoryComic.filterCategory)
-    
-    
-    const [load, setLoad] = useState(false)
-    const dispatch = useDispatch()
-    const inputText = useRef(text)
-    
-    const lengthOfComics = comicStore
-    console.log( lengthOfComics)
+    const [load, setLoad] = useState(false);
+    const dispatch = useDispatch();
+    const inputText = useRef(text);
 
-    const [pages, setPages] = useState(1)
-     const next = () => {
-        setPages(pages + 1)
-    
-    } 
+    const lengthOfComics = comicStore;
+    console.log(lengthOfComics);
+
+    const [pages, setPages] = useState(1);
+    const next = () => {
+        setPages(pages + 1);
+    };
     const prev = () => {
-        setPages(pages-1)
-    }
-  
-    
+        setPages(pages - 1);
+    };
 
-    useEffect( () => {
-                dispatch(
-                    getComics({
-                        inputText: inputText.current?.value,
-                        inputCategory: inputCategory.join(","),
-                        
-                        pages: pages
+    useEffect(() => {
+        dispatch(
+            getComics({
+                inputText: inputText.current?.value,
+                inputCategory: inputCategory.join(","),
+
+                pages: pages,
             })
-        )
-                
-    },[load, inputCategory,pages])
+        );
+    }, [load, inputCategory, pages]);
 
-  /*    const seeMore = () => {
+    /*    const seeMore = () => {
          const limit = comicStore.comics?.comics.length
          dispatch(
             getComics({
@@ -69,8 +64,8 @@ const {getComics} = comicsActions
          }
  } */
 
-    return ( 
-        <main>
+    return (
+        <main className="main">
             <div className="main-background">
                 <div className="section2">
                     <h1>COMICS</h1>
@@ -86,17 +81,13 @@ const {getComics} = comicsActions
                         />
                     </div>
                 </div>
-                
             </div>
             <div className="comics-container">
                 <div className="explore-container">
-                    <h2>
-                        Explore
-                    </h2>
+                    <h2>Explore</h2>
                 </div>
                 <div className="ranges-container">
                     <div className="ranges-1">
-                        
                         <p>Adventures</p>
                     </div>
                     <div className="ranges-2">
@@ -106,34 +97,31 @@ const {getComics} = comicsActions
                         <p>Popular</p>
                     </div>
                 </div>
-                <div className="categories-container">
-                    <ComicsCategories/>
-                </div>
+                
+                    <ComicsCategories />
+                
                 <div className="container-cards">
-                     <ComicsCards/> 
-                      
+                    <ComicsCards />
                 </div>
                 <div className="container-button">
                     <div>
-                        {pages === 1 ? null :(
-                            <button onClick={prev} className='botonnextprev'>prev</button>
+                        {pages === 1 ? null : (
+                            <button onClick={prev} className="botonnextprev">
+                                prev
+                            </button>
                         )}
                     </div>
                     <div>
                         {lengthOfComics?.length < 1 ? null : (
-                        <button onClick={next} className='botonnextprev'>next</button> 
+                            <button onClick={next} className="botonnextprev">
+                                next
+                            </button>
                         )}
-
                     </div>
-
                 </div>
-                
-              
-                
-
             </div>
         </main>
     );
-}
+};
 
-export default Maincomics
+export default Maincomics;
