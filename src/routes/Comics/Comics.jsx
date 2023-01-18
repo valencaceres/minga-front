50a178b1eaffc4112879ac1fbc33e90bf9b2d6e0
Satch5918 <satch_elfante@hotmail.com>
2023-01-18 10:29:33 -0300

    comics+comic+chapter

--- src/routes/Comics/Comics.jsx
+++ src/routes/Comics/Comics.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navcomics from "../../components/navcomics/Navcomics"
 import Maincomics from "./maincomics/Maincomics"
 import "./comics.css"
+import NavBar from '../pages/NavBar'
 
 
 function Comics() {
     return (
         <>
         <div className='nav-container' >
-            <Navcomics/>
+            <NavBar/>
         </div>    
             <Maincomics />
            
