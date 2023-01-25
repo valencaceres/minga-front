8c54facc04a67cdf576d09233d577fc85269998d
valencaceres <vcaceres102@gmail.com>
2023-01-25 10:52:31 -0300

    edit alert

--- src/components/alerts/Alerts.jsx
+++ src/components/alerts/Alerts.jsx
@@ -5,40 +5,28 @@ import 'react-toastify/dist/ReactToastify.css'
  
 
 export default function Alerts() {
-/*     const store = useSelector(store =>  console.log(store)) */
     let view = useSelector(store => store.alertReducer.view)
     let messages = useSelector(store => store.alertReducer.messages)
     console.log(view)
-/*     const alerta = () => {
-      toast('notificacion', {  
-        position: "top-center",
-        autoClose: false,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        })
-    } */
     if(view){
-      toast(typeof messages === "string"? messages : messages = messages?.map(message => message.message).join(),
-      {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        })
-    }
+    typeof messages === "string"? messages : messages = messages?.map(message => message.message).join()
+    Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    )
+    }/* else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })
+    } */
 
   return (
 
     <div>
-        <ToastContainer/>
 
     </div>
 
